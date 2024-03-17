99958a4a324735abd0b09c3d87a839bffca86f1c
Arturo Luna <arturo.luna@stockviz.com>
2024-03-17 19:05:33 +0100

    finish

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [MatTooltipModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
