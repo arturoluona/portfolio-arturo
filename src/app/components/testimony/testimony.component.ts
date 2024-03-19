f8864bea89b7a5eadfee065bcbddbe4c9ae7b576
Arturo Luna <arturo.luna@stockviz.com>
2024-03-19 11:21:38 +0100

    add seb testimony

--- src/app/components/testimony/testimony.component.ts
+++ src/app/components/testimony/testimony.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-testimony',
   standalone: true,
-  imports: [MatIconModule],
+  imports: [MatIconModule, MatButtonModule],
   templateUrl: './testimony.component.html',
   styleUrl: './testimony.component.scss'
 })
@@ -14,8 +15,10 @@ export class TestimonyComponent {
 
   austinTextEs = 'Desde el primer día de trabajo, Arturo ha sido un activo para el equipo. Aporta un sólido conjunto de habilidades en desarrollo front-end, incluido un profundo conocimiento de HTML, CSS, JavaScript y tecnologías relacionadas. Su capacidad para traducir diseños en código con píxeles perfectos es insuperable y constantemente entregó trabajos de alta calidad a tiempo. <br><br> Una de sus principales fortalezas es su carácter colaborativo. Siempre está dispuesto a trabajar con otros miembros del equipo para resolver problemas complejos y brindar comentarios para garantizar que el producto final cumpla con los requisitos funcionales y de diseño. Su voluntad de aprender y crecer es impresionante y siempre está buscando nuevas formas de mejorar sus habilidades y ampliar su conocimiento sobre él. <br><br> Estoy muy impresionado con su trabajo. Su talento, profesionalismo y compromiso con la excelencia lo han convertido en un miembro invaluable del equipo. No tengo dudas de que seguirá sobresaliendo en su trabajo y logrará grandes cosas en su carrera.'
 
-  austinTransslate = false
+  sebTextEn = "I have the pleasure of working with Arturo, whose expertise in Angular, Python, and Django significantly contributes to our project's success. Not only does he bring a very large dose of technical knowledge, but his leadership and problem-solving abilities are outstanding. Arturo's clear and structured communication ensures smooth project progress and his can-do attitude inspires our team to strive for excellence. He's an invaluable asset, always ready to lend a hand and guide others. I highly recommend Arturo for any team looking to elevate their tech capabilities and foster a collaborative working environment."
 
-  sebTransslate = false
+  sebTextEs = "Tengo el placer de trabajar con Arturo, cuya experiencia en Angular, Python y Django contribuye significativamente al éxito de nuestro proyecto. No sólo aporta una gran dosis de conocimiento técnico, sino que su liderazgo y su capacidad de resolución de problemas son sobresalientes. La comunicación clara y estructurada de Arturo garantiza un progreso fluido del proyecto y su actitud positiva inspira a nuestro equipo a esforzarse por alcanzar la excelencia. Es un activo invaluable, siempre dispuesto a echar una mano y guiar a los demás. Recomiendo ampliamente a Arturo para cualquier equipo que busque elevar sus capacidades tecnológicas y fomentar un entorno de trabajo colaborativo."
+
+  translateText = false
 
 }
